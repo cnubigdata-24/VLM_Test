210e64be2ccc4e1568b0aad355b1a160a759f657
Paul's Repository <cnubigdata@gmail.com>
2026-05-24 02:23:47 +0900

    Update 02_image_captioning.py

--- 02_image_captioning.py
+++ 02_image_captioning.py
@@ -5,6 +5,6 @@ from PIL import Image
 
 # 이미지 사진
 img = Image.open("demo.jpg").convert("RGB")
-captioner = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+captioner = pipeline("image-text-to-text", model="Salesforce/blip-image-captioning-base")
 
 print(captioner(img, max_new_tokens=30))
