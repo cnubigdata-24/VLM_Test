1240e330b78d2948615ee8c0b19cf8961b53558c
Paul's Repository <cnubigdata@gmail.com>
2026-05-24 02:18:24 +0900

    Update 01_VQA.py

--- 01_VQA.py
+++ 01_VQA.py
@@ -3,10 +3,8 @@
 from transformers import pipeline 
 from PIL import Image 
 
-# 이미지 경로
 img = Image.open("demo.jpg").convert("RGB") 
 vqa = pipeline("visual-question-answering", model="dandelin/vilt-b32-finetuned-vqa")
 
-question = "What is the person holding?" 
-
+question = "What is the person holding?"
 print(vqa(image=img, question=question))
