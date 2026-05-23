dc32f91a18ea125380ce55c5d8f829c4b2095349
Paul's Repository <cnubigdata@gmail.com>
2026-05-24 02:23:15 +0900

    Update 01_VQA.py

--- 01_VQA.py
+++ 01_VQA.py
@@ -9,6 +9,5 @@ vqa = pipeline("visual-question-answering", model="dandelin/vilt-b32-finetuned-v
 question = "What is the person holding?"
 print(vqa(image=img, question=question))
 
-
-# The image processor of type `ViltImageProcessor` is now loaded as a fast processor by default, even if the model checkpoint was saved with a slow processor. This is a breaking change and may produce slightly different outputs. To continue using the slow processor, instantiate this class with `use_fast=False`. 
+# Result:
 # [{'score': 0.9782189726829529, 'answer': 'cat'}, {'score': 0.01485508494079113, 'answer': 'nothing'}, {'score': 0.0146604860201478, 'answer': 'remote'}, {'score': 0.01343337818980217, 'answer': 'remote control'}, {'score': 0.008481381461024284, 'answer': 'kitten'}]
